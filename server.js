6b0dc6955ab28296c9ed6bd78c4d9aecdb9bb174
ahmed ibrahim <ahmedibrahimhassan654@gmail.com>
2020-02-18 20:40:29 +0200

    add-comment in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require("./config/db");
 const app = express();
 const port = process.env.PORT || 5000;
-
+//db connection
 connectDB();
 app.use(express.json({ extended: false }));
 app.get('/', (req, res) => { res.send('api running '); });
